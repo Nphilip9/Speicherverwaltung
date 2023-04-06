871be1c9c4dbe8ba6671ab6d7531bded88572f2a
Nphilip9 <philip.neumair2020@gmail.com>
2023-04-06 17:37:07 +0200

    began to add function for adding a process that needs multiple bits

--- src/main.c
+++ src/main.c
@@ -5,25 +5,65 @@
 
 NODE *head = NULL;
 
-void createNewElement(int data, char *name);
+void createNewElement(int, char *);
 void printList();
 
 int main() {
-    createNewElement(12, "Hallo");
+    printf("64-byte Ram-Verwaltung\n");
+    printf("1. Prozess hinzufuegen\n");
+    printf("2. Prozess loeschen\n");
+    printf("3. BitMap ausgeben\n");
+
+    int selected;
+    scanf("%d", &selected);
+    switch (selected) {
+        case 1:
+            printf("Wie viel Speicher moechtest du verwenden: ");
+            int name, size, algorithm;
+            scanf("%d", &size);
+            printf("\nWie soll der Prozess heissen (waehle eine Zahl als Name): ");
+            scanf("%d", &name);
+            printf("\n 1. Best Fit");
+            printf("\n 2. First Fit");
+            printf("\n 3. Next Fit");
+            printf("\n 4. Worst Fit");
+            printf("\nWelchen algorithmus moechten sie verwenden: ");
+            scanf("%d", &algorithm);
+            break;
+        case 2:
+            printf("\nWelchen Prozess möchtest du aus dem Speicher entfernen: ");
+            int pName;
+            scanf("%d", &pName);
+            break;
+        case 3:
+            printList();
+            break;
+        default:
+            break;
+    }
     printList();
     return 0;
+}
+
+// TODO:
+void addProcess(int size, int data, char *name) {
+    NODE **tmp = malloc(size * sizeof(NODE));
+    for(int i = 0; i <= size; i++) {
+        createNewElement(data, name);
+    }
 }
 
 void createNewElement(int data, char *name) {
     NODE *newElement = malloc(sizeof(NODE));
     newElement->data = data;
-    strcpy(newElement->name, name);
+    newElement->name = name;
+    newElement->next = NULL;
 
     if(head == NULL) {
         head = newElement;
     } else {
         NODE *current = head;
-        while(current != NULL) {
+        while(current->next != NULL) {
             current = current->next;
         }
         current->next = newElement;
@@ -33,7 +73,7 @@ void createNewElement(int data, char *name) {
 void printList() {
     NODE *current = head;
     while(current != NULL) {
-        printf("%s", current->name);
+        printf("%s\n", current->name);
         current = current->next;
     }
 }
--- src/main.h
+++ src/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN
 #define MAIN
+#define SIZE 64
     typedef struct node {
         int data;
         char *name;
