e7073b924d074f0651e3adc3a07b75bceba56b2f
Nphilip9 <philip.neumair2020@gmail.com>
2023-04-06 19:22:37 +0200

    implemented firstFit bestFit and worstFit algorithms

--- src/main.c
+++ src/main.c
@@ -4,53 +4,20 @@
 #include "main.h"
 
 NODE *head = NULL;
-
-void createNewElement(int, char *);
-void printList();
+int nextFitLastAllocated = 0;
 
 int main() {
-    printf("64-byte Ram-Verwaltung\n");
-    printf("1. Prozess hinzufuegen\n");
-    printf("2. Prozess loeschen\n");
-    printf("3. BitMap ausgeben\n");
-
-    int selected;
-    scanf("%d", &selected);
-    switch (selected) {
-        case 1:
-            printf("Wie viel Speicher moechtest du verwenden: ");
-            int name, size, algorithm;
-            scanf("%d", &size);
-            printf("\nWie soll der Prozess heissen (waehle eine Zahl als Name): ");
-            scanf("%d", &name);
-            printf("\n 1. Best Fit");
-            printf("\n 2. First Fit");
-            printf("\n 3. Next Fit");
-            printf("\n 4. Worst Fit");
-            printf("\nWelchen algorithmus moechten sie verwenden: ");
-            scanf("%d", &algorithm);
-            break;
-        case 2:
-            printf("\nWelchen Prozess möchtest du aus dem Speicher entfernen: ");
-            int pName;
-            scanf("%d", &pName);
-            break;
-        case 3:
-            printList();
-            break;
-        default:
-            break;
+    // Allocate memory
+    for (int i = 0; i < SIZE; ++i) {
+        createNewElement(0, "Hole");
     }
+
+    addProcess("P1", 23);
+    addProcess("P2", 45);
+
     printList();
-    return 0;
-}
 
-// TODO:
-void addProcess(int size, int data, char *name) {
-    NODE **tmp = malloc(size * sizeof(NODE));
-    for(int i = 0; i <= size; i++) {
-        createNewElement(data, name);
-    }
+    return 0;
 }
 
 void createNewElement(int data, char *name) {
@@ -70,10 +37,101 @@ void createNewElement(int data, char *name) {
     }
 }
 
+void addProcess(char *newName, int newData) {
+    int firstFitIndex = bestFit(5);
+    printf("%d", firstFitIndex);
+    int index = firstFitIndex;
+
+    int count = 0;
+    NODE *firstElement = head;
+    while(firstElement->next != NULL && count != firstFitIndex) {
+        firstElement = firstElement->next;
+        count++;
+    }
+
+    NODE *currentFitElement = firstElement;
+    printf("%p\n", currentFitElement);
+    while(currentFitElement->next != NULL && index != firstFitIndex + 5) {
+        currentFitElement->data = newData;
+        currentFitElement->name = newName;
+        index++;
+        currentFitElement = currentFitElement->next;
+    }
+}
+
+// FirstFit sucht den ersten frien Speicherplatz der grossgenug ist
+int firstFit(unsigned int size) {
+    int counter = 0;
+    int index = 0;
+    for(NODE *current = head; current != NULL; current = current->next) {
+        if(strcmp(current->name, "Hole") == 0) {
+            counter++;
+            if(counter == size) {
+                return index + 1 - size;
+            }
+        } else {
+            counter = 0;
+        }
+        index++;
+    }
+}
+
+int bestFit(unsigned int pSize) {
+    int bestBlockIndex = -1;
+    int smallestBlockSize = 0;
+    int index = 0;
+    for (NODE *current = head; current != NULL; current = current->next) {
+        if (strcmp(current->name, "Hole") == 0) {
+            int blockSize = 0;
+            int j = index;
+            // This loop counts each consecutive free block
+            // It runs until the end of the list is reached or until the block size is greater than the requested size
+            NODE *tmp = current;
+            while (tmp != NULL && strcmp(tmp->name, "Hole") == 0 && blockSize < pSize) {
+                blockSize++;
+                tmp = tmp->next;
+                j++;
+            }
+            // This if block checks if the block size is greater than or equal to the requested size and smaller than the current smallest block size found so far
+            // If the condition is true, the smallest block size is updated and the index of the first block of the smallest size is also saved
+            if (blockSize >= pSize && (smallestBlockSize == 0 || blockSize < smallestBlockSize)) {
+                bestBlockIndex = index;
+                smallestBlockSize = blockSize;
+            }
+        }
+        index++;
+    }
+    // Returns the index of the first block of the smallest size found
+    return bestBlockIndex;
+}
+
+// WorstFit sucht den größtmöglichen freien Speicherplatz
+int worstFit(unsigned int size) {
+    int worstBlockIndex = -1;
+    int largestBlockSize = 0;
+    int index = 0;
+    for(NODE *current = head; current != NULL; current = current->next) {
+        if (current->data == 0) {
+            int blockSize = 0;
+            int j = index;
+            while (j != SIZE && current->data == 0) {
+                blockSize++;
+                j++;
+            }
+            if (blockSize >= size && blockSize > largestBlockSize) {
+                worstBlockIndex = index;
+                largestBlockSize = blockSize;
+            }
+        }
+        index++;
+    }
+    return worstBlockIndex;
+}
+
 void printList() {
     NODE *current = head;
     while(current != NULL) {
-        printf("%s\n", current->name);
+        printf("Name: %s | Inhalt: %d | Addresse: %p\n", current->name, current->data, current);
         current = current->next;
     }
 }
--- src/main.h
+++ src/main.h
@@ -6,4 +6,13 @@
         char *name;
         struct node *next;
     } NODE;
+
+    extern void createNewElement(int, char *);
+    extern void printList();
+    extern void addProcess(char *, int);
+
+    extern int bestFit(unsigned int);
+    extern int firstFit(unsigned int);
+    extern int nextFit(unsigned int);
+    extern int worstFit(unsigned int);
 #endif
